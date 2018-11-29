ce38b0562e9feff1882ba70beca41cffcb606520
Bryce Kehoe <bryce@kehoe.co.nz>
2018-11-29 14:39:28 +1300

    Added a link to FishDetails to FishList

--- client/components/Fish.jsx
+++ client/components/Fish.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function Fish (props) {
   return (
-    <li>{props.fishData.name}</li>
+    <li>
+      <Link to={`/fish/${props.fishData.id}`}>
+        {props.fishData.name}
+      </Link>
+    </li>
   )
 }
