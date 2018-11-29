df0d5ca52d4793921aebcfa6856b6586181f056a
Bryce Kehoe <bryce@kehoe.co.nz>
2018-11-29 13:25:32 +1300

    Linting fix

--- client/components/FishList.jsx
+++ client/components/FishList.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import {connect} from 'react-redux'
 
-import { getFish, getSortedFish } from '../actions'
+import {getFish, getSortedFish} from '../actions'
 import Fish from './Fish'
 
 class FishList extends React.Component {
